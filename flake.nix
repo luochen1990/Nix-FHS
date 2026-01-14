d91853a6291fc71391d7aa9f5c11ca75d61e60f7
LuoChen <luochen1990@gmail.com>
2026-01-14 21:22:54 +0800

    chore: tidy flake.nix

--- flake.nix
+++ flake.nix
@@ -4,24 +4,18 @@
   outputs =
     { self, nixpkgs, ... }:
     let
-
-      lib = nixpkgs.lib;
-      utils' = lib // (import ./lib/list.nix) // (import ./lib/dict.nix) // (import ./lib/file.nix);
+      utils' =
+        nixpkgs.lib // (import ./lib/list.nix) // (import ./lib/dict.nix) // (import ./lib/file.nix);
       inherit (import ./lib/prepare-lib.nix utils') prepareLib;
-      utils = prepareLib {
-        roots = [ ./. ];
-        lib = lib;
-      };
+      inherit
+        (prepareLib {
+          roots = [ ./. ];
+          lib = nixpkgs.lib;
+        })
+        mkFlake
+        ;
     in
-    utils.mkFlake {
-      roots = [ ./. ];
-      supportedSystems = [ "x86_64-linux" ];
+    mkFlake {
       inherit self nixpkgs;
-      inputs = self.inputs;
-    }
-    // {
-      # Provide lib and mkFlake outputs for backward compatibility with templates
-      lib = utils;
-      mkFlake = utils.mkFlake;
     };
 }
